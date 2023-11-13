246e8aea78bb35e28d6cc1f9f54be5b2997551d6
Wais Kamal <waisekamal1@gmail.com>
2023-11-13 08:29:44 +0300

    Fixed port issue

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import utils from './utils.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const port = 3000
+const port = process.env.PORT || 3000
 
 const app = express()
 app.use(session({
