c55b2a4959d87ac35c91bddfab8c2bc9548b1dac
Wais Kamal <waisekamal1@gmail.com>
2023-11-14 08:33:56 +0300

    Updated app key

--- client/src/App.vue
+++ client/src/App.vue
@@ -19,7 +19,7 @@ const channelAuthEndpoint = "/pusher/auth"
 const playerDataEndpoint = "/api/player"
 
 // App key
-const APP_KEY = "7ac779fdc52b29158df6"
+const APP_KEY = "a3fff4c418b83c2b12db"
 
 const pusher = new Pusher(APP_KEY, {
   userAuthentication: {
