d90db7ade8b1d02c7134afe3bbb95d4859e717ec
Wais Kamal <waisekamal1@gmail.com>
2023-11-14 08:30:19 +0300

    Removed sensitive app keys from server code

--- api/index.js
+++ api/index.js
@@ -26,11 +26,16 @@ app.use(express.static("public", {
     index: false
 }))
 
+const APP_ID = process.env.APP_ID
+const APP_KEY = process.env.APP_KEY
+const APP_SECRET = process.env.APP_SECRET
+const APP_CLUSTER = process.env.APP_CLUSTER
+
 const pusher = new Pusher({
-    appId: "1677874",
-    key: "7ac779fdc52b29158df6",
-    secret: "10c67e4d409540dee173",
-    cluster: "ap2",
+    appId: APP_ID,
+    key: APP_KEY,
+    secret: APP_SECRET,
+    cluster: APP_CLUSTER,
     forceTLS: true,
 });
 
